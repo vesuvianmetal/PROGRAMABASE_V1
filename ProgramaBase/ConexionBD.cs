4fe91ea0b5e7348c00fa94a1d564339f3460d6b4
vesuvianmetal <vesuvianmetal@hotmail.com>
2018-06-26 16:58:13 -0600

    First Commit: Login window and mysql satabase conection class

---
+++ ProgramaBase/ConexionBD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// This class is to establish a conection to a mysql database
+/// </summary>
+
+/// importing mysql libraries
+using MySql.Data.MySqlClient;
+
+namespace ProgramaBase
+{
+
+    /// <summary>
+    /// name of class for the database conection
+    /// </summary>
+    class ConexionBD
+    {
+        /// <summary>
+        /// mysql conection method
+        /// </summary>
+        /// <returns></returns>
+        public static MySqlConnection ObtenerConexion()
+        {
+            try
+            {
+                ///establish primary database parameters
+                MySqlConnection Conectar = new MySqlConnection("Server =192.168.1.74;port=3306; database = electronicax ; Userid = root; password =; sslmode=none;Allow Zero Datetime=True;Convert Zero Datetime=True;");
+                return Conectar;
+            }
+            catch
+            {
+                ///establish secondary database parameters
+                MySqlConnection Conectar = new MySqlConnection("Server =192.168.1.74;port=3306; database = electronicax ; Userid = root; password =; sslmode=none;Allow Zero Datetime=True;Convert Zero Datetime=True;");
+                return Conectar;
+            }
+
+
+        }
+
+
+
+    }
+}
+
---
+++ ProgramaBase/Login.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ProgramaBase
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(178, 338);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(245, 167);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(100, 22);
+            this.txtuser.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(245, 211);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(100, 22);
+            this.txtpass.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 488);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+    }
+}
+
